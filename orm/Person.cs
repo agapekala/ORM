01d579412bf39988432ca44fec243d97d943252e
apekala <agapekala1@wp.pl>
2019-02-02 18:42:36 +0100

    primary key

---
+++ orm/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using orm.Attributes;
+
+namespace orm
+{
+    [Table("Person")]
+    class Person
+    {
+        [Column("id")]
+        [PrimaryKey()]
+        private int _id { get; set; }
+        [Column("imie")]
+        private string _name { get; set; }
+
+        [Column("nazwisko")]
+        private string _lastname { get; set; }
+        public Person(int id, string name, string lastname)
+        {
+            _id = id;
+            _name = name;
+            _lastname = lastname;
+        }
+    }
+}
