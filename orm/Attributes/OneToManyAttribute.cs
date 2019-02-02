df2525abe66d2cabbb14982f1f342c5c405b2ea0
kartytko <kartytko@gmail.com>
2019-02-02 19:37:23 +0100

    Adding basic relationship attributes.

---
+++ orm/Attributes/OneToManyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace orm.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    class OneToManyAttribute : Attribute
+    {
+        public OneToManyAttribute() {
+        }
+
+    }
+}
---
+++ orm/Attributes/OneToOneAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace orm.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    class OneToOneAttribute : Attribute
+    {
+        public OneToOneAttribute() {
+        }
+    }
+}
