bab70265dde3442002144c0c5d2b909f8448d796
Elżbieta Krzeczek <krzeczekelzbieta@gmail.com>
2019-02-13 12:13:05 +0100

    Update Criteria.cs

--- orm/Criteria/Criteria.cs
+++ orm/Criteria/Criteria.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,10 +6,45 @@ namespace orm.Criteria
 {
     class Criteria
     {
-        object value;
-        string fieldName;
+        private string sqlOperator;
+        protected string field;
+        protected object value;
+        public static List<Criteria> listOfCriteria;
 
-        //public string isEqual() {}
-
+        public Criteria(string sqlOperator, string field, object value)
+        {
+            this.sqlOperator = sqlOperator;
+            this.field = field;
+            this.value = value;
+        }
+        public Criteria(){}
+        public static Criteria greaterThan(string fieldName, object value)
+        {
+            Criteria criteria=new Criteria(">", fieldName, value);
+            listOfCriteria.Add(criteria);
+            return criteria;
+        }        
+        public static Criteria lessThan(string fieldName, object value)
+        {
+            Criteria criteria=new Criteria("<", fieldName, value);
+            listOfCriteria.Add(criteria);
+            return criteria;
+        }        
+        public static Criteria equals(string fieldName, object value)
+        {
+            Criteria criteria=new Criteria("=", fieldName, value);
+            listOfCriteria.Add(criteria);
+            return criteria;
+        }        
+        public static Criteria notEquals(string fieldName, object value)
+        {
+            Criteria criteria=new Criteria("!=", fieldName, value);
+            listOfCriteria.Add(criteria);
+            return criteria;
+        }      
+        public string generateSql()
+        {
+            return sqlOperator + value;
+        }
     }
 }
